28fcf170e23dbede4d8dfb163232f53980cf0be8
Daler.Bakhriev <Daler.Bakhriev@x5.ru>
2021-07-31 14:30:54 +0300

    Wrong version with special channel for finishing goroutine

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"log"
 	"sync"
 )
 
@@ -9,10 +10,19 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func singleTaskWorker(workerNum int, taskInput <-chan Task, taskOutput chan<- error) {
-	for task := range taskInput {
-		err := task()
-		taskOutput <- err
+func singleTaskWorker(workerNum int, taskInput <-chan Task, taskOutput chan<- error, done <-chan struct{}) {
+
+	for {
+		select {
+		case <-done:
+			log.Println("Got signal for finishing")
+			return
+		case task := <-taskInput:
+			err := task()
+			log.Printf("Sending error %v", err)
+			taskOutput <- err
+		}
+
 	}
 }
 
@@ -21,14 +31,17 @@ func Run(tasks []Task, n, m int) error {
 	errorsCh := make(chan error, len(tasks))
 	tasksCh := make(chan Task, len(tasks))
 	wg := &sync.WaitGroup{}
-
+	finishChannels := make([]chan struct{}, 0, n)
 	for workerNum := 1; workerNum < n+1; workerNum++ {
 		wg.Add(1)
-		go func(workerNumber int) {
+		finishCh := make(chan struct{}, 1)
+		go func() {
 			defer wg.Done()
-			singleTaskWorker(workerNumber, tasksCh, errorsCh)
-		}(workerNum)
+			singleTaskWorker(workerNum, tasksCh, errorsCh, finishCh)
+		}()
+		finishChannels = append(finishChannels, finishCh)
 	}
+	log.Printf("Finish channels list is %v", finishChannels)
 
 	for _, task := range tasks {
 		tasksCh <- task
@@ -42,7 +55,13 @@ func Run(tasks []Task, n, m int) error {
 		if taskErr != nil {
 			numErrors++
 		}
+		log.Printf("Errors number is %d", numErrors)
 		if numErrors == m && m > 0 {
+			log.Printf("Errors number is %d, finishing workers with %d channels", m, len(finishChannels))
+			for _, ch := range finishChannels {
+				log.Printf("Sending finish signal for channel %v", ch)
+				ch <- struct{}{}
+			}
 			return ErrErrorsLimitExceeded
 		}
 		numFinishedTasks++
