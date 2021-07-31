99ab4242b5396edfa806cac82a32beb8b8b1986c
Daler.Bakhriev <Daler.Bakhriev@x5.ru>
2021-07-31 18:48:31 +0300

    Fixed errors, added test without sleeps, doesnt work yet

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"log"
 	"sync"
 )
 
@@ -10,65 +9,43 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func singleTaskWorker(workerNum int, taskInput <-chan Task, taskOutput chan<- error, done <-chan struct{}) {
-
-	for {
-		select {
-		case <-done:
-			log.Println("Got signal for finishing")
-			return
-		case task := <-taskInput:
-			err := task()
-			log.Printf("Sending error %v", err)
-			taskOutput <- err
-		}
-
-	}
-}
-
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	errorsCh := make(chan error, len(tasks))
 	tasksCh := make(chan Task, len(tasks))
 	wg := &sync.WaitGroup{}
-	finishChannels := make([]chan struct{}, 0, n)
-	for workerNum := 1; workerNum < n+1; workerNum++ {
-		wg.Add(1)
-		finishCh := make(chan struct{}, 1)
-		go func() {
-			defer wg.Done()
-			singleTaskWorker(workerNum, tasksCh, errorsCh, finishCh)
-		}()
-		finishChannels = append(finishChannels, finishCh)
+	mu := &sync.Mutex{}
+	if n > len(tasks) {
+		n = len(tasks)
 	}
-	log.Printf("Finish channels list is %v", finishChannels)
+	var errorsNum int
+	for workerNum := 0; workerNum < n; workerNum++ {
+		wg.Add(1)
+		go func(numErrors *int) {
+			defer wg.Done()
+			for task := range tasksCh {
+				err := task()
+				mu.Lock()
+				if *numErrors == m && m > 0 {
+					mu.Unlock()
+					return
+				}
+				if err != nil {
+					*numErrors++
+				}
+				mu.Unlock()
+			}
+		}(&errorsNum)
+	}
 
 	for _, task := range tasks {
 		tasksCh <- task
 	}
 	close(tasksCh)
-
-	numErrors := 0
-	numFinishedTasks := 0
-	for numFinishedTasks < len(tasks) {
-		taskErr := <-errorsCh
-		if taskErr != nil {
-			numErrors++
-		}
-		log.Printf("Errors number is %d", numErrors)
-		if numErrors == m && m > 0 {
-			log.Printf("Errors number is %d, finishing workers with %d channels", m, len(finishChannels))
-			for _, ch := range finishChannels {
-				log.Printf("Sending finish signal for channel %v", ch)
-				ch <- struct{}{}
-			}
-			return ErrErrorsLimitExceeded
-		}
-		numFinishedTasks++
-	}
-
 	wg.Wait()
-	close(errorsCh)
+
+	if errorsNum == m && m > 0 {
+		return ErrErrorsLimitExceeded
+	}
 
 	return nil
 }
