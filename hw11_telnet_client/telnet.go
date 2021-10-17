5cd4ba4b712d6f28af990fdf48b2d5322b1ac8bb
Daler.Bakhriev <Daler.Bakhriev@x5.ru>
2021-10-17 23:40:53 +0300

    homework 11 finished

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,58 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type SimpleTelnetClient struct {
+	Address string
+	Timeout time.Duration
+	In      io.ReadCloser
+	Out     io.Writer
+	Conn    net.Conn
+}
+
+func (s *SimpleTelnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", s.Address, s.Timeout)
+	if err != nil {
+		return fmt.Errorf("Connect: %w", err)
+	}
+	s.Conn = conn
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (s *SimpleTelnetClient) Close() error {
+	if s.Conn != nil {
+		if err := s.Conn.Close(); err != nil {
+			return fmt.Errorf("Close: %w", err)
+		}
+	}
+	return nil
+}
+
+func (s *SimpleTelnetClient) Send() error {
+	// s.Out.Write([]byte("I'm telnet client\n"))
+	if _, err := io.Copy(s.Conn, s.In); err != nil {
+		return fmt.Errorf("Send: %w", err)
+	}
+
+	// s.Out.Write([]byte("I will be back!\n"))
+	return nil
+}
+
+func (s *SimpleTelnetClient) Receive() error {
+	if _, err := io.Copy(s.Out, s.Conn); err != nil {
+		return fmt.Errorf("Receive: %w", err)
+	}
+
+	// s.Out.Write([]byte("Bye-bye\n"))
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	// Place your code here.
+	return &SimpleTelnetClient{
+		Address: address,
+		Timeout: timeout,
+		In:      in,
+		Out:     out,
+	}
+}
